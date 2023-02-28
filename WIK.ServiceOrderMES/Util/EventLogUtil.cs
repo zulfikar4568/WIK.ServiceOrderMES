e3b6452f23742290ef9735888708e2665dffec69
zulfikar4568 <isnaen70@gmail.com>
2023-02-28 13:07:21 +0700

    fix: event log util substring

--- WIK.ServiceOrderMES/Util/EventLogUtil.cs
+++ WIK.ServiceOrderMES/Util/EventLogUtil.cs
@@ -25,7 +25,7 @@ namespace WIK.ServiceOrderMES.Util
 
             foreach (EventLog oEventLog in EventLog.GetEventLogs())
             {
-                if (sEventLog.Substring(0, 3).ToUpper() == oEventLog.Log.Substring(0, 3).ToUpper())
+                if (sEventLog.ToUpper() == oEventLog.Log.ToUpper())
                 {
                     sEventLog = oEventLog.Log;
                     break;
